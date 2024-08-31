d3048038b7f173d99931322ecd96fd0650b80077
tina067 <tinamurmu222@gmail.com>
2024-08-31 22:37:18 +0530

    31-08-2024 3rd  commit

---
+++ Questions/1st week/Stack/q2.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void func(vector<int>&arr){
+//     vector<int>ans;
+//     stack<int>s;
+//     int n = arr.size();
+//     for(int i=0; i<n; i++){
+//         if(s.size() == 0)
+//         ans.push_back(-1);
+//         else if(s.size()>0 && s.top() > arr[i])
+//         ans.push_back(s.top());
+//         else if(s.size()>0 && s.top()<=arr[i]){
+//             while(s.size()>0 && s.top() <=arr[i])
+//             s.pop();
+
+//             if(s.size() == 0)
+//             ans.push_back(-1);
+//             else 
+//             ans.push_back(s.top());
+//         }
+//         s.push(arr[i]);
+//     }
+
+//     for(auto i : ans){
+//         cout<< i << " ";
+//     }
+// }
+
+
+void func(vector<int>&arr){
+    int n = arr.size();
+    vector<int>ans;
+    for(int i=0; i<n; i++){
+        int flag =0; 
+        for(int j=i-1; j>=0; j--){
+            if(arr[j]>arr[i]){
+              ans.push_back(arr[j]);
+              flag = 1;
+              break;
+            }
+        }
+        if(flag==0){
+            ans.push_back(-1);
+        }
+    }
+    for(auto i : ans){
+        cout<< i << " ";
+    }
+}
+
+int main(){
+    vector<int>arr = {1, 3, 2, 4};
+    func(arr);
+    return 0;
+}
